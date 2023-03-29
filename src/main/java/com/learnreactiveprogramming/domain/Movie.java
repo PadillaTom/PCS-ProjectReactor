537d30e485e181414cb80419e5538bf5b169fe97
Tomas Padilla <padillatomasagustin@gmail.com>
2023-03-29 17:10:51 +0200

    main - Flux and Mono quick view.

--- src/main/java/com/learnreactiveprogramming/domain/Movie.java
+++ src/main/java/com/learnreactiveprogramming/domain/Movie.java
@@ -10,12 +10,12 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Movie {
-    private MovieInfo movieInfo;
+    private MovieInfo movie;
     private List<Review> reviewList;
     private Revenue revenue;
 
-    public Movie(MovieInfo movieInfo, List<Review> reviewList) {
-        this.movieInfo = movieInfo;
+    public Movie(MovieInfo movie, List<Review> reviewList) {
+        this.movie = movie;
         this.reviewList = reviewList;
     }
 
--- src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,5 +1,29 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
-    //TODO - Video 12
+
+    public Flux<String> namesFlux() {
+        // fromIterable (Should be a DB or RemoteService call)
+        return Flux.fromIterable(List.of("Alex", "Ben", "Tom"));
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("Alex");
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name -> System.out.println("Flux Name is: " + name));
+
+        fluxAndMonoGeneratorService.nameMono()
+                .subscribe(name -> System.out.println("Mono Name is: " + name));
+
+    }
 }
