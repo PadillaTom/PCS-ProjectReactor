41d4215c5ddcdab0ac93e94417084c9f5b3d72e8
Tomas Padilla <padillatomasagustin@gmail.com>
2023-03-29 17:03:06 +0200

    main - Comenzamos video 12

--- src/main/java/com/learnreactiveprogramming/service/MovieInfoService.java
+++ src/main/java/com/learnreactiveprogramming/service/MovieInfoService.java
@@ -1,7 +1,6 @@
 package com.learnreactiveprogramming.service;
 
 import com.learnreactiveprogramming.domain.MovieInfo;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -12,34 +11,6 @@ import static com.learnreactiveprogramming.util.CommonUtil.delay;
 
 public class MovieInfoService {
 
-    private WebClient webClient;
-
-    public MovieInfoService(WebClient webClient) {
-        this.webClient = webClient;
-    }
-
-    public MovieInfoService(){
-
-    }
-
-    public Flux<MovieInfo> retrieveAllMovieInfo_RestClient(){
-
-        return webClient.get().uri("/v1/movie_infos")
-                .retrieve()
-                .bodyToFlux(MovieInfo.class)
-                .log();
-
-    }
-
-    public Mono<MovieInfo> retrieveMovieInfoById_RestClient(Long movieInfoId){
-
-        return webClient.get().uri("/v1/movie_infos/{id}", movieInfoId)
-                .retrieve()
-                .bodyToMono(MovieInfo.class)
-                .log();
-
-    }
-
     public  Flux<MovieInfo> retrieveMoviesFlux(){
 
         var movieInfoList = List.of(new MovieInfo(100l, "Batman Begins", 2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
@@ -48,7 +19,6 @@ public class MovieInfoService {
 
         return Flux.fromIterable(movieInfoList);
     }
-
 
     public  Mono<MovieInfo> retrieveMovieInfoMonoUsingId(long movieId){
 
--- src/main/java/com/learnreactiveprogramming/service/ReviewService.java
+++ src/main/java/com/learnreactiveprogramming/service/ReviewService.java
@@ -1,44 +1,11 @@
 package com.learnreactiveprogramming.service;
 
-import com.learnreactiveprogramming.domain.MovieInfo;
 import com.learnreactiveprogramming.domain.Review;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
 
 public class ReviewService {
-
-    private WebClient webClient;
-
-    public ReviewService(WebClient webClient) {
-        this.webClient = webClient;
-    }
-
-    public ReviewService() {
-    }
-
-    public Flux<Review> retrieveAllReviews_RestClient(){
-        return webClient.get().uri("/v1/reviews")
-                .retrieve()
-                .bodyToFlux(Review.class)
-                .log();
-
-    }
-
-    public Flux<Review> retrieveReviewsFlux_RestClient(Long movieInfoId) {
-
-        var uri = UriComponentsBuilder.fromUriString("/v1/reviews")
-                .queryParam("movieInfoId", movieInfoId)
-                .buildAndExpand()
-                .toUriString();
-
-        return webClient.get().uri(uri)
-                .retrieve()
-                .bodyToFlux(Review.class)
-                .log();
-    }
 
     public  List<Review> retrieveReviews(long movieInfoId){
 
@@ -52,6 +19,5 @@ public class ReviewService {
                 new Review(2L, movieInfoId, "Excellent Movie", 9.0));
         return Flux.fromIterable(reviewsList);
     }
-
 
 }
